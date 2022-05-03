c78f8565c9ac5a6438d93f7f4fb3c30e8a86f8e1
OceanShape <oceanshape700@gmail.com>
2022-05-03 20:03:01 +0900

    Init: Add project

---
+++ photo-frame/photo-frame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photo-frame
+//
+//  Created by 허태양 on 2022/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ photo-frame/photo-frameUITests/photo_frameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  photo_frameUITestsLaunchTests.swift
+//  photo-frameUITests
+//
+//  Created by 허태양 on 2022/05/03.
+//
+
+import XCTest
+
+class photo_frameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
